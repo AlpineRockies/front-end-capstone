d8cc9a4828d3659a7c1774a5092fd6df4d9764b8
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-29 19:18:35 -0400

    Add stub component files

---
+++ client/src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// Here is out base App component.
+// Notice we are NOT using jsx here. This is because we have not set up babel yet.
+const App = React.createElement('h1', null, 'Hello World');
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+// Bring React in to build a component.
+import React from 'react';
+// Bring reactDOM in to mount component to the dom.
+import reactDOM from 'react-dom';
+
+import App from './components/App.jsx'
+
+
+// Render our app to the dom mounted to the element with id of root inside our public/index.html file.
+reactDOM.render(App, document.getElementById('root'));
