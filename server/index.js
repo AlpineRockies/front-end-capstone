7b829b2cb3241be088648430fa27286b6972395b
positivefx <sfy2368@gmail.com>
2022-03-30 16:48:13 -0400

    Fixed query string

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const axios = require('axios');
 
 const app = express();
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.all('/*', (req, res) => {
