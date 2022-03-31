a9efc119cca1f52bc73d3dfa8837b5c9e265a002
Stephanie Yeong <sfy2368@gmail.com>
2022-03-31 14:53:08 -0400

    component folder: ratings and reviews (#14)
    * creating components in app
    
    * setting up server with express and axios
    
    * Added query string to request handlers
    
    * Fixed query string
    
    * Example get req
    
    * updated sample.env
    
    * updated env in webpack
    
    * included default 3000 port in webpack
    
    * Adding rating reviews folder and ratingreview.jsx
    
    * uncommented rating and review

---
+++ client/src/components/RatingReviews/RatingReviews.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+function RatingReviews() {
+
+
+  return (
+    <h2>in rating reviews</h2>
+  )
+}
+
+export default RatingReviews;
