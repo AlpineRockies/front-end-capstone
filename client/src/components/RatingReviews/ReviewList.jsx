25d8f4a9160866dc3eba2c7db8a706af79be5653
Stephanie Yeong <sfy2368@gmail.com>
2022-04-01 10:53:29 -0400

    Adding RR components  (#19)
    * creating components in app
    
    * setting up server with express and axios
    
    * Added query string to request handlers
    
    * Fixed query string
    
    * Example get req
    
    * updated sample.env
    
    * updated env in webpack
    
    * included default 3000 port in webpack
    
    * Adding rating reviews folder and ratingreview.jsx
    
    * uncommented rating and review
    
    * installed momentjs in package.json
    
    * Added useeffect App
    
    * RR basic review entry, sort and modal
    
    * Revert "Added useeffect App"
    
    This reverts commit f8cfda18337c137c05c01a418da12301a2e4a8d3.

---
+++ client/src/components/RatingReviews/ReviewList.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useContext } from "react";
+import ReviewEntry from "./ReviewEntry.jsx";
+
+function ReviewList({ sortedList }) {
+  return (
+    <div className="RR-review-list">
+      <h4>Review List </h4>
+      {sortedList.map((eachReview) => (
+        <ReviewEntry key={eachReview.review_id} eachReview={eachReview} />
+      ))}
+    </div>
+  );
+}
+
+export default ReviewList;
