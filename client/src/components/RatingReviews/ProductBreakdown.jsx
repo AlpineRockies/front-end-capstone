5496a5a2a965834861ad1c0b8446bc2228b21605
positivefx <sfy2368@gmail.com>
2022-04-01 21:43:33 -0400

    creating and connecting star and product breakdown

---
+++ client/src/components/RatingReviews/ProductBreakdown.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ProductBreakdown() {
+
+
+  return(
+    <div>
+      <h2> product breakdown</h2>
+    </div>
+  )
+
+}
+
+export default ProductBreakdown;
---
+++ client/src/components/RatingReviews/StarBreakdown.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function StarBreakdown() {
+
+  return(
+    <div>
+      <h2> star breakdown</h2>
+    </div>
+  )
+
+}
+
+export default StarBreakdown;
