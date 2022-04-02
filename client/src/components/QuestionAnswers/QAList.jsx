f1bb4fa572fd6d2a84f575811b8db0ec1e67aac2
Addison Hernandez <addison.hernandez@gmail.com>
2022-04-02 08:56:41 -0400

    Render up to four questions initially
     - Add the ability to show more questions if a product has more than four
    questions.
     - Update the list of questions automatically if `productId` changes.

--- client/src/components/QuestionAnswers/QAList.jsx
+++ client/src/components/QuestionAnswers/QAList.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import axios from 'axios';
 import React, { useContext, useState, useEffect } from 'react';
 import ProductContext from '../Context';
@@ -7,32 +6,51 @@ import QAListEntry from './QAListEntry';
 function QAList() {
   const { productId } = useContext(ProductContext);
 
-  const [questions, setQuestions] = useState(() => []);
-  const [render, setRender] = useState(() => (
-    <p className="loading">Loading ...</p>
-  ));
+  const initialQuestionCount = 4;
+
+  const [questions, setQuestions] = useState([]);
+  const [shownQuestionCount, setShownQuestionCount] = useState(initialQuestionCount);
+  const [renderedItems, setRenderedItems] = useState(<p className="loading">Loading ...</p>);
 
   // get list of questions
   useEffect(() => {
     axios
       .get(`/qa/questions?product_id=${productId}`)
       .then((response) => response.data.results)
-      .then((newQuestions) => setQuestions(newQuestions))
+      .then((newQuestions) => {
+        setQuestions(newQuestions);
+        setShownQuestionCount(initialQuestionCount);
+      })
+      // eslint-disable-next-line no-console
       .catch(console.error);
-  }, []);
+  }, [productId]);
 
-  // update
-  useEffect(
-    () => setRender(
-      questions.length && questions.map((question) => (
-        <QAListEntry key={question.question_id} questionData={question} />
-      )),
-    ),
-    [questions],
-  );
+  // update rendered questions
+  useEffect(() => {
+    if (questions.length) {
+      const renderedQuestions = questions.slice(0, shownQuestionCount);
+
+      setRenderedItems(
+        renderedQuestions.map((question) => (
+          <QAListEntry key={question.question_id} questionData={question} />
+        )),
+      );
+    }
+  }, [questions, shownQuestionCount]);
+
+  const handleMoreQuestions = () => setShownQuestionCount(questions.length);
 
   // map over list of questions and render a list entry for each
-  return <div className="QA-list">{render}</div>;
+  return (
+    <div className="QA-list">
+      {renderedItems}
+      {shownQuestionCount < questions.length ? (
+        <button type="button" onClick={handleMoreQuestions}>
+          More Answered Questions
+        </button>
+      ) : null}
+    </div>
+  );
 }
 
 export default QAList;
