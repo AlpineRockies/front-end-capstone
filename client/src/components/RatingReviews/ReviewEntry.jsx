a9a6af95fdbcc567b3e7f2703078333fcf9df0bc
positivefx <sfy2368@gmail.com>
2022-04-06 16:27:54 -0400

    put request working

--- client/src/components/RatingReviews/ReviewEntry.jsx
+++ client/src/components/RatingReviews/ReviewEntry.jsx
@@ -1,18 +1,42 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { HiThumbUp, HiThumbDown } from 'react-icons/hi';
 import moment from 'moment';
+import axios from 'axios';
 import ReviewPhotos from './ReviewPhotos';
 
 function ReviewEntry({ eachReview }) {
+  const [helpfulClick, setHelpfulClick] = useState(false);
+  const [notHelpfulClick, setNotHelpfulClick] = useState(false);
+  const [reportClick, setReportClick] = useState(false);
 
-  const handleClickHelpful = () => {};
+  const handleHelpful = (value) => {
+    if (value === true) {
+      axios
+        .put(`/reviews/${eachReview.review_id}/helpful`)
+        .then((response) => console.log(response))
+        .then(() => setHelpfulClick(true))
+        .catch((err) => {
+          console.log('err in RR helpful PUT');
+        });
+    } else {
+      setNotHelpfulClick(true);
+    }
+  };
 
-  const handleClickReport = () => {};
+  const handleReport = () => {
+    reportClick ||
+      axios
+        .put(`/reviews/${eachReview.review_id}/report`)
+        .then(() => setReportClick(true))
+        .catch((err) => {
+          console.log('err in RR report PUT');
+        });
+  };
 
   return (
     <div className="RR-review-entry">
       <div className="RR-review-photos">
-        {eachReview.photos[0] && <ReviewPhotos photos={eachReview.photos} /> }
+        {eachReview.photos[0] && <ReviewPhotos photos={eachReview.photos} />}
       </div>
       <div className="RR-review-header">
         Rating = {eachReview.rating}
@@ -24,10 +48,36 @@ function ReviewEntry({ eachReview }) {
       </div>
       <div className="RR-review-person-response">
         <p>{eachReview.reviewer_name}</p>
-        <p>{eachReview.response}</p>
+        {eachReview.response && (
+          <p>Response from seller:{eachReview.response}</p>
+        )}
       </div>
       <div className="RR-helpful">
-        <p>Helpfulness? {eachReview.helpfulness} </p>
+        <p>Was this review helpful?</p>
+        <label>
+          {helpfulClick ? (
+            eachReview.helpfulness + 1
+          ) : (
+            <HiThumbUp onClick={() => handleHelpful(true)} />
+          )}
+        </label>
+        <label>
+          {notHelpfulClick ? (
+            eachReview.helpfulness - 1
+          ) : (
+            <HiThumbDown onClick={() => handleHelpful(false)} />
+          )}
+        </label>
+      </div>
+      <br />
+      <div className="RR-report">
+        <span
+          onClick={handleReport}
+          role="button"
+          style={{ cursor: 'pointer', textDecoration: 'underline' }}
+        >
+          {reportClick ? 'Reported' : 'Report'}
+        </span>
       </div>
     </div>
   );
