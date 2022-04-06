a396774e196c4563ea5db069f2b7872c752a831f
positivefx <sfy2368@gmail.com>
2022-04-05 22:23:58 -0400

    fixing post in write review

--- client/src/components/RatingReviews/WriteReview.jsx
+++ client/src/components/RatingReviews/WriteReview.jsx
@@ -10,7 +10,6 @@ function WriteReview({
   productId,
   metaData,
 }) {
-
   const [starRating, setStarRating] = useState(0);
   const [summaryRating, setSummaryRating] = useState(null);
   const [bodyRating, setBodyRating] = useState(null);
@@ -56,91 +55,117 @@ function WriteReview({
     setCharacteristicRating(characteristics);
   };
 
+  const overlayStyle = {
+    position: 'fixed',
+    zIndex: 1020,
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    background: 'rgba(255, 255, 255, 0.75)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+  const modalStyle = {
+    background: 'white',
+    width: '45rem',
+    maxWidth: 'calc(100vw - 2rem)',
+    maxHeight: 'calc(100vh - 2rem)',
+    overflowY: 'auto',
+    position: 'relative',
+    border: '1px solid #ccc',
+    borderRadius: '0.3rem',
+  };
+
   return (
     <div className="RR-wr-form">
       {showWriteReview ? (
-        <form onSubmit={handleReviewInputSubmit}>
-          <h4>Write Your Review</h4>
-          <div className="RR-wr-exit">
-            <button
-              className="RR-wr-exit-button"
-              type="button"
-              onClick={() => setShowWriteReview(false)}
-            >
-              X
-            </button>
-          </div>
+        <div style={overlayStyle}>
+          <div style={modalStyle}>
+            <form onSubmit={handleReviewInputSubmit}>
+              <h4>Write Your Review</h4>
+              <div className="RR-wr-exit">
+                <button
+                  className="RR-wr-exit-button"
+                  type="button"
+                  onClick={() => setShowWriteReview(false)}
+                >
+                  X
+                </button>
+              </div>
 
-          <div className="RR-wr-content">
-            <div className="RR-wr-stars">
-              <WriteReviewStar handleStar={handleStar} />
-            </div>
-            <div className="RR-wr-recommend">
-              <span>Would you recommend?</span>
-              <label>
-                <HiThumbUp onClick={() => setRecommendRating(true)} />
-              </label>
-              <label>
-                <HiThumbDown onClick={() => setRecommendRating(false)} />
-              </label>
-            </div>
-            <div className="RR-wr-characteristic">
-              <WriteReviewCharacteristic
-                handleCharacteristic={handleCharacteristic}
-                metaData={metaData}
-              />
-            </div>
-            <div className="RR-wr-name">
-              <input
-                placeholder="Name"
-                type="text"
-                name="name"
-                onChange={(event) => setNameRating(event.target.value)}
-              />
-            </div>
-            <div className="RR-wr-email">
-              <input
-                placeholder="Email"
-                type="text"
-                name="email"
-                onChange={(event) => setEmailRating(event.target.value)}
-              />
-            </div>
-            <div className="RR-wr-summary">
-              <input
-                placeholder="Summary"
-                type="text"
-                name="summary"
-                maxLength="60"
-                onChange={(event) => setSummaryRating(event.target.value)}
-              />
-            </div>
-            <div className="RR-wr-body">
-              <input
-                placeholder="Review"
-                type="text"
-                name="body"
-                maxLength="250"
-                onChange={(event) => setBodyRating(event.target.value)}
-              />
-            </div>
-            <div className="RR-wr-photo">
-              <input
-                placeholder="Link to photos"
-                type="text"
-                name="photo"
-                onChange={(event) =>
-                  setPhotoRating([...photoRating, event.target.value])
-                }
-              />
-            </div>
-            // inner break for content
+              <div className="RR-wr-content">
+                <div className="RR-wr-stars">
+                  <WriteReviewStar handleStar={handleStar} />
+                </div>
+                <div className="RR-wr-recommend">
+                  <span>Would you recommend?</span>
+                  <label>
+                    <HiThumbUp onClick={() => setRecommendRating(true)} />
+                  </label>
+                  <label>
+                    <HiThumbDown onClick={() => setRecommendRating(false)} />
+                  </label>
+                </div>
+                <div className="RR-wr-characteristic">
+                  <WriteReviewCharacteristic
+                    handleCharacteristic={handleCharacteristic}
+                    metaData={metaData}
+                  />
+                </div>
+                <div className="RR-wr-name">
+                  <input
+                    placeholder="Name"
+                    type="text"
+                    name="name"
+                    onChange={(event) => setNameRating(event.target.value)}
+                  />
+                </div>
+                <div className="RR-wr-email">
+                  <input
+                    placeholder="Email"
+                    type="text"
+                    name="email"
+                    onChange={(event) => setEmailRating(event.target.value)}
+                  />
+                </div>
+                <div className="RR-wr-summary">
+                  <input
+                    placeholder="Summary"
+                    type="text"
+                    name="summary"
+                    maxLength="60"
+                    onChange={(event) => setSummaryRating(event.target.value)}
+                  />
+                </div>
+                <div className="RR-wr-body">
+                  <input
+                    placeholder="Review"
+                    type="text"
+                    name="body"
+                    maxLength="250"
+                    onChange={(event) => setBodyRating(event.target.value)}
+                  />
+                </div>
+                <div className="RR-wr-photo">
+                  <input
+                    placeholder="Link to photos"
+                    type="text"
+                    name="photo"
+                    onChange={(event) =>
+                      setPhotoRating([...photoRating, event.target.value])
+                    }
+                  />
+                </div>
+                // inner break for content
+              </div>
+              <button className="RR-wr-submit-button" type="submit">
+                Submit Review
+              </button>
+            </form>
           </div>
-
-          <button className="RR-wr-submit-button" type="submit">
-            Submit Review
-          </button>
-        </form>
+        </div>
       ) : null}
     </div>
   );
