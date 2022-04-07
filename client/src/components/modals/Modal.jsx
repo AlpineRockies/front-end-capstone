c40cdcad93994117f6edab54d82bef495a914a20
Addison Hernandez <addison.hernandez@gmail.com>
2022-04-07 12:34:35 -0400

    Update CSS styles
    Now with more glassmorphism!

--- client/src/components/modals/Modal.jsx
+++ client/src/components/modals/Modal.jsx
@@ -16,10 +16,11 @@ function Modal({
     // top, left, transform: centers the div
     top: '50%',
     left: '50%',
-    transform: 'translate(-50%, -50%)',
+    transform: 'translate( -50%, -50% )',
     padding: '1em',
-    borderRadius: '5px',
-    backgroundColor: '#FFF',
+    borderRadius: '8px',
+    background: 'rgba( 255, 255, 255, 0.87 )',
+    boxShadow: '0 8px 32px 0 rgba( 32, 32, 32, 0.32 )',
     zIndex: 255,
   };
 
@@ -29,8 +30,8 @@ function Modal({
     bottom: 0,
     left: 0,
     right: 0,
-    background: 'rgba( 0, 0, 0, 0.5 )',
-    backdropFilter: 'blur( 4px )',
+    background: 'rgba( 50, 50, 50, 0.5 )',
+    backdropFilter: 'blur( 10px )',
     zIndex: 254,
   };
 
