55e1af21f7c18ebccc4cd327157f68e770f66508
Addison Hernandez <addison.hernandez@gmail.com>
2022-04-07 12:34:35 -0400

    Add CSS styling and update title

--- client/src/components/QuestionAnswers/QuestionAnswers.jsx
+++ client/src/components/QuestionAnswers/QuestionAnswers.jsx
@@ -5,9 +5,17 @@ import QAList from './QAList';
 function QuestionAnswers() {
   const [searchFilter, setSearchFilter] = useState('');
 
+  const qaMainStyle = {
+    position: 'relative',
+    left: '50vw',
+    transform: 'translateX(-50%)',
+    maxWidth: '960px',
+    width: '87vw',
+  };
+
   return (
-    <div className="qa-main">
-      <h1>Q and A</h1>
+    <div className="qa-main" style={qaMainStyle}>
+      <h1>Questions and Answers</h1>
       <Search onSearch={(query) => setSearchFilter(query)} />
       <QAList filter={searchFilter} />
     </div>
