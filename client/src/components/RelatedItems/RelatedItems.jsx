cca486f14d4730361af377ad296b2153b6b7e3eb
Will Atwood <x1@galaxia.us>
2022-04-08 14:38:26 -0400

    Code refactor for _.reduce.  Moving functionality to Utility directory.

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable no-return-assign */
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import _ from 'underscore';
 import './style.css';
+import { aveRating } from 'Utilities';
 import Related from './RelatedProducts/Related';
 import YourOutfit from './YourOutfit/YourOutfit';
 import ProductContext from '../Context';
@@ -56,18 +58,15 @@ function RelatedItems() {
   }, [relatedItems]);
 
   useEffect(() => {
-    const aveRating = _.map(relatedReviews, (product) => ({
-      aveRating:
-        _.reduce(product.results, (sum, num) => (sum + num.rating), 0) /
-        product.results.length,
-    }));
+    const aveRatingArray = _.map(relatedReviews, (product) => (
+      { aveRating: aveRating(product.results) }));
 
     const accArr = _.map(relatedItems, (ea, index) =>
       _.extend(
         {},
         relatedItemsImg[index],
         relatedItemsDetails[index],
-        aveRating[index],
+        aveRatingArray[index],
       )
     );
     setJoinedAPIDetails(accArr);
@@ -81,14 +80,10 @@ function RelatedItems() {
       axios
         .all([getYOImages, getYODetails, getYOReviews])
         .then((response) => {
-          const aveRating = {
-            aveRating: _.reduce(response[2].data.results, (sum, num) => (
-              sum + num.rating
-            ), 0) / response[2].data.results.length
-          }
+          const ratingsArray = response[2].data.results;
           setYourOutfit([
             ...yourOutfit,
-            { ...response[0].data, ...response[1].data, ...aveRating },
+            { ...response[0].data, ...response[1].data, ...{ aveRating: aveRating(ratingsArray) } },
           ]);
         })
         .catch((err) => console.log(err));
@@ -110,3 +105,11 @@ function RelatedItems() {
 }
 
 export default RelatedItems;
+
+
+
+// const aveRating = _.map(relatedReviews, (product) => ({
+//   aveRating:
+//     _.reduce(product.results, (sum, num) => (sum + num.rating), 0) /
+//     product.results.length,
+// }));
