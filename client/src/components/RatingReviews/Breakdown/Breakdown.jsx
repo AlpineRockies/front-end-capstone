bde2645e35c9013a39a8a983bad70db14aa972eb
positivefx <sfy2368@gmail.com>
2022-04-09 11:17:16 -0400

    removeding range style

--- client/src/components/RatingReviews/Breakdown/Breakdown.jsx
+++ client/src/components/RatingReviews/Breakdown/Breakdown.jsx
@@ -7,22 +7,6 @@ import { FaStar } from 'react-icons/fa';
 
 const Input = styled.input
 `
-  background: #00aec9;
-  color: #fff;
-  cursor: pointer;
-  margin-bottom: 0;
-  text-transform: uppercase;
-  width: 100%;
-  border-radius: 5px;
-  height: 35px;
-  border-color: transparent;
-  box-shadow: 0px;
-  outline: none;
-  transition: 0.15s;
-  text-align: center;
-  &:active {
-    background-color: #f1ac15;
-  }
 `;
 
 function Breakdown({ metaData, handleStarReviewClick }) {
