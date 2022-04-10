a8fcbc3fc464d04f503282f2d8f63812d5a3094e
positivefx <sfy2368@gmail.com>
2022-04-09 22:50:06 -0400

    commenting out input files

--- client/src/components/RatingReviews/WriteReview/WriteReviewPhoto.jsx
+++ client/src/components/RatingReviews/WriteReview/WriteReviewPhoto.jsx
@@ -51,3 +51,73 @@ function WriteReviewPhoto({ photoRating, setPhotoRating }) {
 }
 
 export default WriteReviewPhoto;
+
+
+// import React, { useState, useEffect, useRef } from 'react';
+
+// function WriteReviewPhoto({ photoRating, setPhotoRating }) {
+//   const [showUploadPhoto, setShowUploadPhoto] = useState(false);
+//   const [imageArray, setImageArray] = useState([]);
+//   const [showUploadButton, setShowUploadButton] = useState(true);
+// console.log(photoRating);
+
+//   useEffect(() => {
+//     handleUploadButton();
+//     if (imageArray.length < 1) {
+//       return;
+//     }
+//     imageArray.map((image) =>
+//       setPhotoRating([...photoRating, URL.createObjectURL(image)])
+//     );
+//   }, [imageArray]);
+
+//   const handleImgInput = (event) => {
+//     setImageArray([...event.target.files]);
+//   };
+
+//   const handleUploadButton = () => {
+//     if (photoRating.length >= 4) {
+//       setShowUploadButton(false);
+//     }
+//   };
+
+//   const imgThumb = {
+//     height: '60px',
+//     width: '60px',
+//     borderRadius: '50px',
+//     padding: '10px',
+//     flex: '1 20%',
+//   };
+
+//   return (
+//     <div>
+//       <button
+//         className="RR-wr-upload-photo"
+//         type="button"
+//         onClick={() => setShowUploadPhoto(true)}
+//       >
+//         Upload Photos
+//       </button>
+//       {showUploadPhoto ? (
+//         <div>
+//           {showUploadButton && (
+//             <input
+//               type="file"
+//               multiple
+//               accept="image/*"
+//               onChange={handleImgInput}
+//             />
+//           )}
+//           {photoRating.map((imgSrc) => (
+//             <label key={imgSrc}>
+//               <img src={imgSrc} alt="imgSrc" style={imgThumb} />
+//             </label>
+//           ))}
+
+//         </div>
+//       ) : null}
+//     </div>
+//   );
+// }
+
+// export default WriteReviewPhoto;
