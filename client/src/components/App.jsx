c6550b7ee0ea425365a48954161538f7cc9958e6
akb3y <alisha.barraw@gmail.com>
2022-04-13 15:10:26 -0500

    added a header, did some css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,6 +6,7 @@ import RelatedItems from './RelatedItems/RelatedItems';
 import QuestionAnswers from './QuestionAnswers/QuestionAnswers';
 import RatingReviews from './RatingReviews/RatingReviews';
 import ProductContext from './Context';
+import Header from './Header/Header';
 
 function App() {
   const [productId, setProductId] = useState(38320);
@@ -49,7 +50,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Team Alpine Rockies!</h1>
+      <Header />
       <ProductContext.Provider value={memoizedState}>
         <Overview avgRating={avgRating} />
         <RelatedItems />
