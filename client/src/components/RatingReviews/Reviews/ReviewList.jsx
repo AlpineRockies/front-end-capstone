ea738f5cc0a0679010edc610f499effb82860456
positivefx <sfy2368@gmail.com>
2022-04-13 14:22:16 -0400

    changes to reviewlist gtom overview

--- client/src/components/RatingReviews/Reviews/ReviewList.jsx
+++ client/src/components/RatingReviews/Reviews/ReviewList.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
-=======
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
->>>>>>> 22404a98d908f1ced3693dad67dd1b3371aa5e8c
 import ReviewEntry from './ReviewEntry';
 import ReviewSearch from './ReviewSearch';
 import { StyledButton } from '../Style/RatingReviewStyle';
