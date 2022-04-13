0c39016752452284efa8bab7a3d313fe9357ffca
positivefx <sfy2368@gmail.com>
2022-04-13 14:24:27 -0400

    star entry merge conflict

--- client/src/components/RatingReviews/Reviews/ReviewEntryStar.jsx
+++ client/src/components/RatingReviews/Reviews/ReviewEntryStar.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import { starFillPercentage } from 'Utilities';
 
 function ReviewEntryStar({ rating }) {
-
   const starsInner = {
-<<<<<<< HEAD
     backgroundImage: `linear-gradient(90deg, var(--ebony) ${starFillPercentage(rating)}%, var(--dutch-white) 0.1%, var(--dutch-white) ${100 - starFillPercentage(rating)}%)`,
-=======
->>>>>>> 22404a98d908f1ced3693dad67dd1b3371aa5e8c
     backgroundClip: 'text',
-    backgroundImage: `linear-gradient(90deg, var(--dutch-white) ${starFillPercentage(rating)}%, var(--ebony) 0.1%, var(--ebony) ${100 - starFillPercentage(rating)}%)`,
     WebkitBackgroundClip: 'text',
     color: 'transparent',
   };
